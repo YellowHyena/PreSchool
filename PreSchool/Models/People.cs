2d545ab0d017c91e0a517663ec483dedd532ae44
Max <98maxekh@gafe.molndal.se>
2021-12-23 10:15:52 +0100

    removed Age

--- PreSchool/Models/People.cs
+++ PreSchool/Models/People.cs
@@ -6,10 +6,6 @@
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public long PersonalNumber { get; set; }
-        public int Age
-        {
-            get { return PeopleHelper.GetAge(PersonalNumber); }
-        }
         public List<Guardian> Guardians { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
@@ -22,15 +18,8 @@
         public int Id { get; set; }
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
-
         public long PersonalNumber { get; set; }
-        public int Age
-        {
-            get { return PeopleHelper.GetAge(PersonalNumber); }
-        }
-
         public List<Child> Children { get; set; }
-
         public string PhoneNumber { get; set; } = "";
 
     }
@@ -40,12 +29,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
-        public long PersonalNumber { get; set; }
-        public int Age
-        {
-            get { return PeopleHelper.GetAge(PersonalNumber); }
-        }       
+        public long PersonalNumber { get; set; } 
         public string PhoneNumber { get; set; } = "";
         public DateTime EmployementDate { get; set; }
         public List<Group> Groups { get; set; }
@@ -64,26 +48,5 @@
         public string PhoneNumber { get; set; } = "";
         public string Group { get; set; } = "";
         public string Child { get; set; } = "";
-    }
-
-    internal class PeopleHelper   //Hallå vart tog referenslänken vägen??? Fixa PLUS ATT DET INTE ENS FUNKAR?????
-    {
-        public static int GetAge(long personalNumber)
-        {
-            int year = (int)((personalNumber / 100000000));
-
-            int month = (int)((personalNumber / 1000000) % 100);
-
-            int day = (int)((personalNumber / 10000) % 100);
-
-            DateTime birth = new DateTime(year, month, day);
-            DateTime today = DateTime.Now.Date;
-            DateTime birthDay = new DateTime(today.Year, month, day);
-
-            int birthdayAdjustment = today < birthDay ? -1 : 0;
-            int age = today.Year - birth.Year + birthdayAdjustment;
-
-            return age;
-        }
     }
 }
--- PreSchool/Utils/StartupTask.cs
+++ PreSchool/Utils/StartupTask.cs
@@ -20,35 +20,7 @@ namespace PreSchool.Utils
             db.Groups.Add(new Group { Name = "Avdelning Tre" });
             db.SaveChanges();
         }
-    //    public static void CreateChildren()
-    //    {
-    //        using var db = new SchoolContext();
 
-    //        if (db.Children.Any()) return;
-    //        db.Children.Add(new Child
-    //        { FirstName = "Knatte",
-    //            LastName = "Anka",
-    //            PersonalNumber = 199011220000,
-
-    //    public int Age
-    //    {
-    //        get { return PeopleHelper.GetAge(PersonalNumber); }
-    //    }
-
-    //    public List<Address> AddressesInfo { get; set; }
-    //    public List<Guardian> Guardians { get; set; }
-    //    public bool PublicPreSchool { get; set; } = false;
-    //    public bool FifteenHours { get; set; } = false;
-    //    public DateTime StartDate { get; set; }
-    //    public DateTime EndDate { get; set; }
-    //    public DateTime ApplicationDate { get; set; }
-    //    public List<Group> Groups { get; set; }
-
-    //    public int BillRecipent { get; set; } //adultid
-    //});
-    //        db.Children.Add(new Child { Name = "Avdelning Två" });
-    //        db.Children.Add(new Child { Name = "Avdelning Tre" });
-    //        db.SaveChanges();
-    //    }
+   
     }
 }
